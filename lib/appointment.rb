83705880b18e4f4b131995eca2a40cda0b3203a5
Angela Mumba <malaika.mumba@gmail.com>
2019-05-31 15:33:56 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,15 @@
+class Appointment 
+  @@all = []
+  
+  def initialize(date,patient,doctor)
+    @name = name 
+    @date = date 
+    @patient = patient 
+    @doctor = doctor 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+end 
